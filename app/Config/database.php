e1a8ff0f7c7cc97c80d07664a2f04dc4a8c04698
Woody <woody180@rocketmail.com>
2021-11-22 13:53:46 +0400

    clear database config params

--- app/Config/database.php
+++ app/Config/database.php
@@ -3,9 +3,9 @@
 define("DATABASE", TRUE);
 define('DATABASE_TYPE', 'MySQL'); // MySQL, PostgreSQL, SQLite, CUBRID
 
-CONST DB_HOST       = 'localhost';
-CONST DB_NAME       = 'magma';
-CONST DB_USERNAME   = 'root';
+CONST DB_HOST       = '';
+CONST DB_NAME       = '';
+CONST DB_USERNAME   = '';
 CONST DB_PASSWORD   = '';
 CONST DB_PORT       = '30000';
 CONST SQLITE_PATH   = APPROOT . '/DB/sqlite.db';
