854dcb406bcc64b33e27c68281f35110289c8fa1
Woody <woody180@rocketmail.com>
2021-12-10 00:52:58 +0400

    form error helper

--- app/Engine/Helpers/engineFormHelpers.php
+++ app/Engine/Helpers/engineFormHelpers.php
@@ -33,3 +33,13 @@ function setForm(array $data) {
 function setMethod(string $method) {
     return '<input name="_method" type="hidden" value="'.$method.'" />';
 }
+
+
+function show_error(string $error_name, string $field_name) {
+
+    if (hasFlashData($error_name)) {
+        return '<p style="color: #f0506e; font-size: .875rem; line-height: 1.5; margin-top: 0; margin-bottom: 0;">'.implode(', ', getFlashData($error_name)->{$field_name} ?? []).'</p>';
+    }
+
+    return '';
+}
